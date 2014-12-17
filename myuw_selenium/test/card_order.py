3132fae165111ff1aed5492393ff5660ec7e6ffd
pmichaud <pmichaud@cmp1.local>
2014-12-16 20:25:51 -0800

    Updating the card order test to actually test it.

--- myuw_selenium/test/card_order.py
+++ myuw_selenium/test/card_order.py
@@ -162,35 +162,69 @@ class CardOrderTest(SeleniumLiveServerTestCase):
         from time import sleep
 
         dates = [
-            "2013-04-01",
-            "2013-06-06",
-            "2013-06-07",
-            "2013-06-08",
-            "2013-06-14",
-            "2013-06-15",
-            "2013-02-02",
-            "2013-02-01",
-            "2013-01-31",
-            "2013-03-10",
-            "2013-03-11",
-            "2013-03-26",
-            "2013-03-27",
-            "2013-04-01",
+            { 'date': "2013-04-01", 'user': 'javerage' },
+            { 'date': "2013-04-25", 'user': 'javerage' }, # Same!
+            { 'date': "2013-04-26", 'user': 'none' }, # Needs to be none to have no registrations, otherwise RegStatusCard is hidden
+            { 'date': "2013-05-30", 'user': 'none' }, # Same!
+            { 'date': "2013-03-10", 'user': 'none' }, # Need to go back in time, otherwise autumn makes this break - Same though
+            { 'date': "2013-03-11", 'user': 'none' }, # Need to go back in time, otherwise autumn makes this break
+            { 'date': "2013-06-07", 'user': 'javerage' }, # Same (ish)!
+            { 'date': "2013-06-08", 'user': 'javerage' },
+            { 'date': "2013-06-13", 'user': 'javerage' }, # Same!
+            { 'date': "2013-06-15", 'user': 'javerage' },
+            { 'date': "2013-08-27", 'user': 'javerage' }, # Need to go to the future - spring's grade submission deadline is always today actual.
+            { 'date': "2013-08-28", 'user': 'javerage' }, # Need to go to the future - spring's grade submission deadline is always today actual.
+            { 'date': "2013-09-24", 'user': 'javerage' }, # Same
+            { 'date': "2013-09-25", 'user': 'javerage' },
         ]
 
+        correct_cards = [
+            [u'FutureQuarterCardA', u'VisualScheduleCard', u'CourseCard', u'HFSCard', u'TuitionCard', u'LibraryCard', u'AcademicCard'],
+            [u'FutureQuarterCardA', u'VisualScheduleCard', u'CourseCard', u'HFSCard', u'TuitionCard', u'LibraryCard', u'AcademicCard'],
+            [u'RegStatusCard', u'VisualScheduleCard', u'CourseCard', u'HFSCard', u'TuitionCard', u'LibraryCard', u'AcademicCard', u'FutureQuarterCard1'],
+            [u'RegStatusCard', u'VisualScheduleCard', u'CourseCard', u'HFSCard', u'TuitionCard', u'LibraryCard', u'AcademicCard', u'FutureQuarterCard1'],
+            [u'RegStatusCard', u'VisualScheduleCard', u'CourseCard', u'HFSCard', u'TuitionCard', u'LibraryCard', u'AcademicCard', u'FutureQuarterCard1'],
+            [u'VisualScheduleCard', u'CourseCard', u'HFSCard', u'TuitionCard', u'LibraryCard', u'AcademicCard', u'FutureQuarterCard1'],
+            [u'FutureQuarterCardA', u'VisualScheduleCard', u'CourseCard', u'HFSCard', u'TuitionCard', u'LibraryCard', u'AcademicCard'],
+            [u'GradeCard', u'FinalExamCard', u'FutureQuarterCardA', u'CourseCard', u'HFSCard', u'TuitionCard', u'LibraryCard', u'AcademicCard'],
+            [u'GradeCard', u'FinalExamCard', u'FutureQuarterCardA', u'CourseCard', u'HFSCard', u'TuitionCard', u'LibraryCard', u'AcademicCard'],
+            [u'GradeCard', u'FutureQuarterCardA', u'CourseCard', u'HFSCard', u'TuitionCard', u'LibraryCard', u'AcademicCard'],
+            [u'GradeCard', u'FutureQuarterCardA', u'CourseCard', u'HFSCard', u'TuitionCard', u'LibraryCard', u'AcademicCard'],
+            [u'GradeCard', u'VisualScheduleCard', u'CourseCard', u'HFSCard', u'TuitionCard', u'LibraryCard', u'AcademicCard', u'FutureQuarterCard1'],
+            [u'GradeCard', u'VisualScheduleCard', u'CourseCard', u'HFSCard', u'TuitionCard', u'LibraryCard', u'AcademicCard', u'FutureQuarterCard1'],
+            [u'VisualScheduleCard', u'CourseCard', u'HFSCard', u'TuitionCard', u'LibraryCard', u'AcademicCard', u'FutureQuarterCard1'],
+        ]
 
-        for date in dates:
+        index = 0
+        for val in dates:
+            date = val["date"]
+            user = val["user"]
+            self.driver.get(self.live_server_url + '/users/')
+            element = self.driver.find_element_by_xpath("//input[@name='override_as']")
+            element.clear()
+            element.send_keys(user)
+            element.submit()
+
             self.driver.get(self.live_server_url + '/mobile/admin/dates/')
             element = self.driver.find_element_by_xpath("//input[@name='date']")
             element.clear()
             element.send_keys(date)
             element.submit()
             self.driver.get(self.live_server_url + '/mobile/landing/')
+            # XXX - this is lame.  need to add something to wait on here instead
             sleep(2)
             title = self.driver.title
             self.assertEquals(self.driver.title, "MyUW Mobile Home")
 
             divs = self.driver.find_elements_by_css_selector("#landing_content > div")
-#            for div in divs:
-#                print "D: ", div.get_attribute("id"), div.get_attribute("style")
 
+            displayed = []
+            for div in divs:
+                if div.get_attribute("style") != "display: none;":
+                    displayed.append(div.get_attribute("id"))
+
+            cards = correct_cards[index]
+            for i in range(0, len(cards)-1):
+                self.assertEquals(cards[i], displayed[i])
+
+            index = index + 1
